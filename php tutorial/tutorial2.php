cc03f6469acdae2973480f60378b553bceb556a8
Carlos Teixeira <c.texeirajr@gmail.com>
2024-10-31 13:57:44 -0300

    treino tutorial php

--- php tutorial/tutorial2.php
+++ php tutorial/tutorial2.php
@@ -7,8 +7,36 @@
 </head>
 <body>
     <h1>PHP Page</h1>
-    <?php 
-        
-    ?>
+    <p>
+        <?php
+            $nome = "Carlos ";
+            $sobrenome = "Teixeira";
+            echo "Olá, meu nome é $nome";
+            echo "Eu posso concatenar meu nome e sobrenome " . $nome . $sobrenome;
+        ?>
+    </p>
+    <p>
+        <?php
+            $y = 5;
+            $x = 6;
+            $soma = $y + $x;
+            //echo "Eu posso somar $y mais $x que dá o resultado: " . ($y + $x);
+            echo "Eu posso somar $y mais $x que dá o resultado: $soma";
+        ?>
+    </p>
+    <p>
+        Testanto var_dump ...
+        <?php 
+            $vetor = [3,4,5,6,"ronaldo"];
+            var_dump($vetor);
+        ?>
+    </p>
+    <p>
+        declarando variáveis com mesmo valor na mesma linha: 
+        <?php 
+            $x = $t = $z = "Olá, ";
+            echo $x . $t . $z;
+        ?>
+    </p>
 </body>
 </html>
